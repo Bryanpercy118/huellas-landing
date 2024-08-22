384d36a367fe72719403300ebf43148d4d2ad6f2
bryanpercy118 <bryanpercy77@gmail.com>
2024-08-22 14:50:20 -0500

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LandingPageComponent } from './features/landing/components/landing-page/landing-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: '/landing', pathMatch:'full'},
+  {path: 'landing', component: LandingPageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LandingPageComponent } from './features/landing/components/landing-page/landing-page.component';
+import { HeaderComponent } from './features/landing/components/header/header.component';
+import { HeroComponent } from './features/landing/components/hero/hero.component';
+import { AboutComponent } from './features/landing/components/about/about.component';
+import { FeaturesComponent } from './features/landing/components/features/features.component';
+import { PricingComponent } from './features/landing/components/pricing/pricing.component';
+import { NewsComponent } from './features/landing/components/news/news.component';
+import { TestimonialsComponent } from './features/landing/components/testimonials/testimonials.component';
+import { FooterComponent } from './features/landing/components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingPageComponent,
+    HeaderComponent,
+    HeroComponent,
+    AboutComponent,
+    FeaturesComponent,
+    PricingComponent,
+    NewsComponent,
+    TestimonialsComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
