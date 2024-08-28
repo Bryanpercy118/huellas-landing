de76cb5859cf79b333fad6cc7fb2020af4cbc62c
bryanpercy118 <bryanpercy77@gmail.com>
2024-08-28 10:57:13 -0500

    change

--- src/app/features/landing/components/Museo/museo/museo.component.ts
+++ src/app/features/landing/components/Museo/museo/museo.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-museo',
   templateUrl: './museo.component.html',
   styleUrls: ['./museo.component.css']
 })
-export class MuseoComponent {
+export class MuseoComponent implements OnInit, AfterViewInit {
 
+  @ViewChild('videoRef') videoElement!: ElementRef<HTMLVideoElement>;
+
+  ngOnInit(): void {
+    this.ngAfterViewInit(); 
+  }
+  ngAfterViewInit(): void {
+    this.playVideo();
+  }
+
+  playVideo(): void {
+    const video: HTMLVideoElement = this.videoElement.nativeElement;
+    
+    // Asegura que el video esté silenciado
+    video.muted = true;
+    
+    // Reproduce el video
+    video.play().catch(error => {
+      console.error('Error al intentar reproducir el video:', error);
+    });
+  }
 }
